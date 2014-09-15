988212b99d2ea25427aecb9bfee4ac8826e7749d
Bryan Potter <potterbm@gmail.com>
2014-09-15 16:04:22 -0600

    debugging

--- js/todo-knockout.js
+++ js/todo-knockout.js
@@ -4,10 +4,13 @@ function Item(text, completed) {
 	var self = this;
 	
 	if(typeof(text) == "object") {
+		console.log(text);
 		var object = $.extend({ text : "", completed : false }, text);
+		console.log(object);
+		console.log(!!object.completed);
 		
 		self.text = ko.observable(object.text);
-		self.completed = ko.observable(object.completed);
+		self.completed = ko.observable(!!object.completed);
 	}
 	
 	else {
